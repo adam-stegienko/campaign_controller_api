cb6a59676379482773b2af25eee9041ea6d49759
Adam Stegienko <adam.stegienko1@gmail.com>
2024-05-06 21:39:06 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# campaign_controller_api
---
+++ src/main/java/com/adam_stegienko/app/App.java
@@ -0,0 +1,13 @@
+package com.adam_stegienko.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
