fd47672931c01a9e0c84bbccfe03da13c0252191
Adam Stegienko <adam.stegienko@blackrack.pl>
2024-05-20 15:07:33 +0200

    Fix: code smells covered up

--- src/main/java/com/adam_stegienko/campaign_controller_api/controller/HomeController.java
+++ src/main/java/com/adam_stegienko/campaign_controller_api/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.adam_stegienko.campaign_controller_api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-  @RequestMapping("/")
+  @GetMapping("/")
   public String campaignControllerMessage() {
     return "Campaign Controller App";
   }
