e0cba8a16cf44d55c8d2448602289854ed8f7574
Adam Stegienko <adam.stegienko@blackrack.pl>
2024-07-15 16:32:19 +0200

    api cors enabled dev added

---
+++ src/main/java/com/adam_stegienko/campaign_controller_api/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.adam_stegienko.campaign_controller_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/v1/api/**")
+                        .allowedOrigins("http://localhost:3000", "https://campaign-controller.stegienko.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
