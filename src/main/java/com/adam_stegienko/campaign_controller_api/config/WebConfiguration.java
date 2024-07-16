e6c1f8023a6718836270aaf5f3afb781388fe41b
Adam Stegienko <adam.stegienko@blackrack.pl>
2024-07-16 15:48:08 +0200

    Limit CORS for localhost to dev profile

--- src/main/java/com/adam_stegienko/campaign_controller_api/config/WebConfiguration.java
+++ src/main/java/com/adam_stegienko/campaign_controller_api/config/WebConfiguration.java
@@ -1,24 +1,39 @@
 package com.adam_stegienko.campaign_controller_api.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration {
 
+    @Autowired
+    private Environment env;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/v1/api/**")
-                        .allowedOrigins("http://localhost:3000", "https://campaign-controller.stegienko.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+            public void addCorsMappings(CorsRegistry registry) {
+                // Check if the 'dev' profile is active
+                if (Arrays.asList(env.getActiveProfiles()).contains("dev")) {
+                    registry.addMapping("/v1/api/**")
+                            .allowedOrigins("http://localhost:3000", "https://campaign-controller.stegienko.com")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                } else {
+                    registry.addMapping("/v1/api/**")
+                            .allowedOrigins("https://campaign-controller.stegienko.com")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*")
+                            .allowCredentials(true);
+                }
             }
         };
     }
